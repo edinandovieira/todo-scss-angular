5a8aaf35a81a1bf3f313af69696e5529931422aa
edinando.vieira <edvieira93@gmail.com>
2023-03-27 14:05:09 -0300

    add sort event checked and validationInput empty option delete

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 //Interface
 import { TaskList } from './../../model/task-list';
@@ -8,14 +8,14 @@ import { TaskList } from './../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [];
 
   constructor() {}
 
-  ngOnInit(): void{
-
+  ngDoCheck(){
+    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
   }
 
   public setEmitTaskList(event: string){
@@ -32,6 +32,16 @@ export class TodoListComponent implements OnInit {
     if(confirm){
       this.taskList = [];
     }    
+  }
+
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja Deletar?");
+
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
   }
 
 }
