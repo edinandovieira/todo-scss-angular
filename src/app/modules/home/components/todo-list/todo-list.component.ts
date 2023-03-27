d181f10e7499fd9f3501188511c0c29f6affd986
edinando.vieira <edvieira93@gmail.com>
2023-03-27 13:25:49 -0300

    add event deleteTask and deleteAllTask

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -21,4 +21,16 @@ export class TodoListComponent implements OnInit {
 
   }
 
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+  
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja realmente Deletar tudo?");
+
+    if(confirm){
+      this.taskList = [];
+    }    
+  }
+
 }
